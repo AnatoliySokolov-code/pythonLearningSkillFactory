dfc4a1781a2bb77535182b1c56c90ab377dcd805
AnatoliySokolov-code <109688326+AnatoliySokolov-code@users.noreply.github.com>
2023-03-16 22:11:38 +0300

    Разбираюсь с модулем 18.3, почти всё заработало

--- module_18.3.py
+++ module_18.3.py
@@ -1,48 +1,42 @@
-#Создание Telegram-бота
+# Создание Telegram-бота
 
 import telebot
 
 TOKEN = "6257486153:AAHtdNEakiHk5-M7Nwk7f1Oad9sCJ-K-DjI"
 
 bot = telebot.TeleBot(TOKEN)
- #Чтобы запустить бота, нужно воспользоваться методом polling.
-
-bot.polling(none_stop=True)
-
-import telebot
-
-TOKEN = "6257486153:AAHtdNEakiHk5-M7Nwk7f1Oad9sCJ-K-DjI"
-
-bot = telebot.TeleBot(TOKEN)
-# Обрабатываются все сообщения, содержащие команды '/start' or '/help'.
-@bot.message_handler(commands=['start', 'help'])
-def handle_start_help(message):
-    pass
-
 
 # Обрабатываются все документы и аудиозаписи
-@bot.message_handler(content_types=['document', 'audio'])
-def handle_docs_audio(message):
-    pass
-#Задание 18.3.1
+@bot.message_handler(content_types=['audio'])
+def handle_audios(message):
+    bot.send_message(message.chat.id, f"Ваш документ получен, {message.chat.username}!")
+#def say_lmao(message: telebot.types.Message):
+#def handle_audio(message: telebot.types.Message):
+    #bot.reply_to(message, 'К сожалению, я не умею слушать и говорить, пишите мне, пожалуйста')
 
-#Допишите обработчик так, чтобы он из сообщения брал username и выдавал
+    # pass
+@bot.message_handler(content_types=['document'])
+def handle_docs(message):
+    #
+    bot.send_message(message.chat.id, f"Ваш документ получен, {message.chat.username}!")
+# @bot.message_handler(filters)
+# def function_name(message):
+    # bot.reply_to(message, "This is a message handler")
+# Задание 18.3.1
+
+# Допишите обработчик так, чтобы он из сообщения брал username и выдавал
 # приветственное сообщение с привязкой к пользователю.
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start', 'help']) 
 def send_welcome(message):
-    bot.send_message(message.chat.id, f"Welcome, \ c{message.chat.username}")
-#Задание 18.3.2
-#Напишите обработчик, который на сообщения с фотографией будет отвечать
+    bot.send_message(message.chat.id, f"Добро пожаловать, {message.chat.username}!")
+
+# Задание 18.3.2
+# Напишите обработчик, который на сообщения с фотографией будет отвечать
 # сообщением «Nice meme XDD». Бот должен отвечать не отдельным сообщением,
 # а с привязкой к картинке.
-import telebot
-
-bot = telebot.TeleBot(TOKEN)
-
-
-@bot.message_handler(content_types=['photo', ])
+@bot.message_handler(content_types=['photo'])
 def say_lmao(message: telebot.types.Message):
-    bot.reply_to(message, 'Nice meme XDD')
+    bot.reply_to(message, 'Красивый МЭМ, ха-ха-ха!')
 
-
+ # Чтобы запустить бота, нужно воспользоваться методом polling. В конце кода
 bot.polling(none_stop=True)
