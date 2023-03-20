81e4d3d15d2b5389ed9d3ff9f1d355aee84e0c8a
AnatoliySokolov-code <109688326+AnatoliySokolov-code@users.noreply.github.com>
2023-03-20 22:02:27 +0300

    Сделал задание 18.4.4

---
+++ task_18.4.4.py
@@ -0,0 +1,31 @@
+#Задание 18.4.4
+#Напишите программу, которая будет с помощью парсера lxml доставать текст
+# из тега tag2 следующего HTML:
+
+#<html>
+ #<head> <title> Some title </title> </head>
+ #<body>
+  #<tag1> some text
+     #<tag2> MY TEXT </tag2>
+   #</tag1>
+ #</body>
+#</html>
+
+# Ответ:
+import lxml.html
+
+html = ''' <html>
+ <head> <title> Some title </title> </head>
+ <body>
+  <tag1> some text
+     <tag2> MY TEXT </tag2>
+   </tag1>
+ </body>
+</html>
+'''
+
+tree = lxml.html.document_fromstring(html)
+
+text = tree.xpath('/html/body/tag1/tag2/text()')
+
+print(text)
